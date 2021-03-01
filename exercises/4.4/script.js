0a47abad282dac76d41444f65127be348b772957
biazidioti <bzferreira.bio@gmail.com>
2021-03-01 13:44:08 -0300

    Adiciona um objeto a chave e atriibui a este objeto um array.

--- exercises/4.4/script.js
+++ exercises/4.4/script.js
@@ -11,8 +11,12 @@ let player = {
     lastName: 'Silva',
     age: 34,
     medals: { golden: 2, silver: 3 },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 };
 
 // Acesse a chave name , lastName e age . Concatene as informações para fazer um console.log no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de indade.');
+
+// Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
