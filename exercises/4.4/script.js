464ef2b8911c3824c59e48dfc737b8849b01224f
biazidioti <bzferreira.bio@gmail.com>
2021-03-01 13:33:45 -0300

    Cria objeto com variaveis.

--- exercises/4.4/script.js
+++ exercises/4.4/script.js
@@ -1,2 +1,16 @@
-// Exercícios sobre objetos
+// ................. Exercícios sobre objetos ........................
+// Crie um objeto player contendo as variáveis listadas abaixo.
 
+// let name = 'Marta';
+//     let lastName = 'Silva';
+//     let age = 34;
+//     let medals = { golden: 2, silver: 3 };
+
+let player = { 
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+};
+
+ console.log("player.name");
