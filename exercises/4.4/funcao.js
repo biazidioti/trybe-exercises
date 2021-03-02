4300eca86de188f79fa72696939af59152d0795d
biazidioti <bzferreira.bio@gmail.com>
2021-03-02 11:18:18 -0300

    Adiciona exercicos de funcao.

---
+++ exercises/4.4/funcao.js
@@ -0,0 +1,8 @@
+// 1. Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function verificaPalindrome('word') {
+    let separateWord = [];
+
+    for (let index = 0; index < word.length; index += 1) {
+        
+    }
