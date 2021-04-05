4100c576cc15bee6a6243db09ec437e6e802b5e9
biazidioti <bzferreira.bio@gmail.com>
2021-04-05 18:46:23 -0300

    Usa .map e . filter no exercicio.

--- exercises/8.3/script.js
+++ exercises/8.3/script.js
@@ -94,3 +94,12 @@ const estudantes = [
 
   console.log(allNameStudents);
 
+  const reportStatus = (name, students) => {
+    const studentInfo = students.find((student) => student.nome === name);
+    return studentInfo.materias.map((materia) => (
+      `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+    ));
+  };
+  
+  console.log(reportStatus('Natalia', estudantes));
+  
